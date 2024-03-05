83d5ce52218bc3605bdcc5639ea0fa4d91bb3c39
Gloria Lim <glorialimartt@gmail.com>
2024-03-05 10:48:19 -0500

    Query number of stores with over 1M revenue

--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -7,4 +7,14 @@ require_relative './exercise_4'
 puts "Exercise 5"
 puts "----------"
 
-# Your code goes here ...
+# Total revenue for the entire company (all stores)
+total_revenue = Store.sum("annual_revenue")
+puts "Total revenue for the company is $#{total_revenue}"
+
+# Average store revenue
+avg_revenue = Store.average("annual_revenue")
+puts "Average store revenue is $#{avg_revenue}"
+
+# number of stores that are generating $1M or more in annual sales
+million_annual_sales = Store.where('annual_revenue > ?', 1000000).count
+puts "Number of stores with over $1M sales: #{million_annual_sales}"
