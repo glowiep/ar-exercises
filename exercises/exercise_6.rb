beee168e22d8a088b3e9e952a2a1c63952d669b8
Gloria Lim <glorialimartt@gmail.com>
2024-03-05 10:58:39 -0500

    Add the association between the Store and Employee table

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -9,3 +9,16 @@ puts "Exercise 6"
 puts "----------"
 
 # One-to-many association
+@store2 = Store.find_by(id: 2)
+
+@store1.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60)
+@store1.employees.create(first_name: "Samantha", last_name: "Bailey", hourly_rate: 60)
+@store1.employees.create(first_name: "Cathy", last_name: "Simons", hourly_rate: 60)
+@store1.employees.create(first_name: "Tyra", last_name: "West", hourly_rate: 60)
+@store1.employees.create(first_name: "Mike", last_name: "Lam", hourly_rate: 60)
+
+@store2.employees.create(first_name: "Earl", last_name: "Holland", hourly_rate: 60)
+@store2.employees.create(first_name: "Indigo", last_name: "Osta", hourly_rate: 60)
+@store2.employees.create(first_name: "Tom", last_name: "Li", hourly_rate: 60)
+@store2.employees.create(first_name: "Summer", last_name: "North", hourly_rate: 60)
+@store2.employees.create(first_name: "Earl", last_name: "Winston", hourly_rate: 60)
