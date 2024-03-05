25f8f9b2f4e330c05d2eb33340f9a02f09c44c72
Gloria Lim <glorialimartt@gmail.com>
2024-03-05 07:53:29 -0500

    Update the first store

--- exercises/exercise_2.rb
+++ exercises/exercise_2.rb
@@ -4,4 +4,7 @@ require_relative './exercise_1'
 puts "Exercise 2"
 puts "----------"
 
-# Your code goes here ...
+# Update the first store
+@store1 = Store.find_by(id: 1)
+@store1.name = "Markham"
+@store1.save
