f8bcf9593b64f8a6f822c7450cabee3fe1958938
Gloria Lim <glorialimartt@gmail.com>
2024-03-05 07:49:41 -0500

    Insert 3 stores into stores table

--- Gemfile
+++ Gemfile
@@ -2,5 +2,5 @@
 source "https://rubygems.org"
 
 gem 'pry' # for debugging
-gem 'pg'
+gem 'pg', '~> 1.3.5'
 gem 'activerecord', '6.1.5.1'
--- exercises/exercise_1.rb
+++ exercises/exercise_1.rb
@@ -3,4 +3,7 @@ require_relative '../setup'
 puts "Exercise 1"
 puts "----------"
 
-# Your code goes below here ...
+# create 3 stores in the database
+Burnaby = Store.create(name: "Burnaby", annual_revenue: 300000, mens_apparel: true, womens_apparel: true)
+Richmond = Store.create(name: "Richmond", annual_revenue: 1260000, mens_apparel: false, womens_apparel: true)
+Gastown = Store.create(name: "Gastown", annual_revenue: 190000, mens_apparel: true, womens_apparel: false)
