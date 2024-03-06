8669f363f1a4ac360216ebf1942505db7b94ea64
Gloria Lim <glorialimartt@gmail.com>
2024-03-05 22:57:12 -0500

    Add validations for both models

--- lib/store.rb
+++ lib/store.rb
@@ -1,3 +1,15 @@
 class Store < ActiveRecord::Base
   has_many :employees
+  validates :name, length: { minimum: 3 }
+  validates :annual_revenue, numericality: { greater_than_or_equal_to: 0 }
+  validate :carry_men_or_women_apparel
+
+  private
+
+  # Stores must carry at least one of the men's or women's apparel
+  def carry_men_or_women_apparel 
+    if !mens_apparel? && !womens_apparel?
+      errors.add(:base, "Must have at least one type of apparel present")
+    end
+  end
 end
